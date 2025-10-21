3b795005030d5820cf4785b67588f0ce6463e6ac
scarpariinside26 <scarpariinside@gmail.com>
2025-10-22 00:07:44 +0200

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://kegosmuokofyqrzwgjrl.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlZ29zbXVva29meXFyendnanJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MjU3OTQsImV4cCI6MjA3NjIwMTc5NH0.5DFSqm3MhGhlQTiEPrU6d62gaRjDq-89coOhBz_gQls'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
