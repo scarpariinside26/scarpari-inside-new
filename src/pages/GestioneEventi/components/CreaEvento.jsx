479d0df1f9cd0e6f6c3db1d12393b0698bb31f31
scarpariinside26 <scarpariinside@gmail.com>
2025-10-24 13:29:55 +0200

    Update CreaEvento.jsx

--- src/pages/GestioneEventi/components/CreaEvento.jsx
+++ src/pages/GestioneEventi/components/CreaEvento.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { supabase } from '../../../supabaseClient';
+import { supabase } from '../../supabaseClient';
 
 function CreaEvento({ onSuccess, onCancel }) {
   const [formData, setFormData] = useState({
