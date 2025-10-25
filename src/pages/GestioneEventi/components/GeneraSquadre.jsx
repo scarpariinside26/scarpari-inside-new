e90796b7c9d645b64f91fa7165e9d705104a9f2c
scarpariinside26 <scarpariinside@gmail.com>
2025-10-25 14:08:43 +0200

    Update GeneraSquadre.jsx

--- src/pages/GestioneEventi/components/GeneraSquadre.jsx
+++ src/pages/GestioneEventi/components/GeneraSquadre.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../../../../supabaseClient';
+import { supabase } from '../../../lib/supabaseClient';  // ✅
 function GeneraSquadre() {
   const [giocatori, setGiocatori] = useState([]);
   const [giocatoriFiltrati, setGiocatoriFiltrati] = useState([]);
