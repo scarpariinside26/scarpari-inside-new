185594bb5e79c8a37ce5ace8e1521a41736a47b9
scarpariinside26 <scarpariinside@gmail.com>
2025-10-25 14:02:48 +0200

    Update GestioneEventi.jsx

--- src/pages/GestioneEventi/GestioneEventi.jsx
+++ src/pages/GestioneEventi/GestioneEventi.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { supabase } from '../../../../supabaseClient';
+import { supabase } from '../../../../lib/supabaseClient';
 import './GestioneEventi.css';
 import CreaEvento from './components/CreaEvento';
 import ListaEventi from './components/ListaEventi';
