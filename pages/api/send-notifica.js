7448a3f8c92ac8fa356fcf19756e446e91a163e3
scarpariinside26 <scarpariinside@gmail.com>
2025-10-25 23:32:38 +0200

    Create send-notifica.js

---
+++ pages/api/send-notifica.js
@@ -0,0 +1,7 @@
+const transporter = nodemailer.createTransport({
+  service: 'gmail',  // ← 'gmail' non 'smtp.ethereal.email'
+  auth: {
+    user: process.env.scarpariinside@gmail.com,
+    pass: process.env.rmpc kzrh kach fyoi
+  }
+});
