c34b5b0aa689086bbf4784aad6c1a20155b7a542
scarpariinside26 <scarpariinside@gmail.com>
2025-10-25 14:11:21 +0200

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+// USA QUESTI VALORI DIRETTAMENTE:
+const supabaseUrl = 'https://kegosmuokofyqrzwgjrl.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlZ29zbXVva29meXFyendnanJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MjU3OTQsImV4cCI6MjA3NjIwMTc5NH0.5DFSqm3MhGhlQTiEPrU6d62gaRjDq-89coOhBz_gQls';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
