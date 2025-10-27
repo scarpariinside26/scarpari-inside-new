b0429122f92713d8f87b0385ca8ddcff7fa90b30
scarpariinside26 <scarpariinside@gmail.com>
2025-10-27 16:58:00 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			runtime: 'nodejs22.x'
-		})
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
