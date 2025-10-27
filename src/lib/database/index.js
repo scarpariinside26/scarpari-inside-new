ac4e8148566be8b6846fd87e8591adee1d1a02db
scarpariinside26 <scarpariinside@gmail.com>
2025-10-27 18:44:34 +0100

    Update index.js

--- src/lib/database/index.js
+++ src/lib/database/index.js
@@ -1,4 +1,5 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { eq } from 'drizzle-orm'; // AGGIUNGI QUESTA RIGA
 import Database from 'better-sqlite3';
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 import * as schema from './schema.js';
