07c7d74956d6a6da3e9ba8839986799ce9865752
scarpariinside26 <scarpariinside@gmail.com>
2025-10-27 18:29:51 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,7 @@
 <script>
-	export let data;
+  import '../app.css';
 </script>
 
-<svelte:head>
-	<meta name="description" content="Scarpari Inside - Il tempio del calcetto" />
-</svelte:head>
-
-<main class="min-h-screen bg-gray-50">
-	<slot />
-</main>
+<div class="min-h-screen bg-gray-50">
+  <slot />
+</div>
